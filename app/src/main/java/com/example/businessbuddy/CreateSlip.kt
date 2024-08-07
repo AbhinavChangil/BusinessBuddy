a76020e6a81660800bf1e3772d0938bb41c8ea6a
AbhinavChangil <e21cseu0910@bennett.edu.in>
2024-08-07 23:38:12 +0530

    Slip PDF genrated and saved in database

--- app/src/main/java/com/example/businessbuddy/CreateSlip.kt
+++ app/src/main/java/com/example/businessbuddy/CreateSlip.kt
@@ -1,8 +1,12 @@
 package com.example.businessbuddy
 
 import android.app.DatePickerDialog
+import android.graphics.Color
+import android.graphics.pdf.PdfDocument
 import android.icu.util.Calendar
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +14,10 @@ import com.example.businessbuddy.databinding.ActivityCreateSlipBinding
 import com.example.businessbuddy.model.Slip
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -47,7 +55,7 @@ class CreateSlip : AppCompatActivity() {
             updateLabel(myCalendar)
         }
 
-        //open date picker on clicking date
+        // Open date picker on clicking date
         binding.edtDate.setOnClickListener {
             DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                 myCalendar.get(Calendar.DAY_OF_MONTH)).show()
@@ -80,72 +88,151 @@ class CreateSlip : AppCompatActivity() {
         }
     }
 
-    //fetch current slip number and automatically update slip number
+    // Fetch current slip number and automatically update slip number
     private fun fetchAndSetCurrentSlipNumber() {
-        availableSlipNumbersReference.addListenerForSingleValueEvent(object : ValueEventListener {
+        slipCountReference.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                    val availableSlipNumbers = snapshot.children.mapNotNull { it.key?.toInt() }
-                    if (availableSlipNumbers.isNotEmpty()) {
-                        binding.numberEditText.setText(availableSlipNumbers.min().toString())
-                        return
-                    }
-                }
-                slipCountReference.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val currentCount = snapshot.getValue(Int::class.java) ?: 0
-                        binding.numberEditText.setText(currentCount.toString())
+                val currentCount = snapshot.getValue(Int::class.java) ?: 0
+                val slipNumber = currentCount + 1
+
+                // Check for unused slip numbers
+                availableSlipNumbersReference.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(availableSnapshot: DataSnapshot) {
+                        if (availableSnapshot.exists()) {
+                            val availableSlipNumbers = availableSnapshot.children.mapNotNull { it.key?.toInt() }
+                            val minUnusedSlipNumber = availableSlipNumbers.minOrNull()
+
+                            if (minUnusedSlipNumber != null && minUnusedSlipNumber < slipNumber) {
+                                binding.numberEditText.setText(minUnusedSlipNumber.toString())
+                            } else {
+                                binding.numberEditText.setText(slipNumber.toString())
+                            }
+                        } else {
+                            binding.numberEditText.setText(slipNumber.toString())
+                        }
                     }
 
                     override fun onCancelled(error: DatabaseError) {
-                        showToast("Failed to fetch slip number: ${error.message}")
+                        showToast("Failed to fetch available slip numbers: ${error.message}")
                     }
                 })
             }
 
             override fun onCancelled(error: DatabaseError) {
-                showToast("Failed to fetch available slip numbers: ${error.message}")
+                showToast("Failed to fetch slip count: ${error.message}")
             }
         })
     }
 
-    //save slip data in database
+    // Save slip data in database
     private fun saveSlip(slipNumber: String, slipDate: String, slipName: String, slipVehicleNo: String, slipItem: String, slipQuantity: String, slipAmount: String) {
         val slip = Slip(slipNumber, slipDate, slipName, slipVehicleNo, slipItem, slipQuantity, slipAmount)
         slipReference.child(slipNumber).setValue(slip).addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                availableSlipNumbersReference.child(slipNumber).removeValue()
-                showToast("Data saved successfully")
-                clearFields()
-                fetchAndSetCurrentSlipNumber()
+                availableSlipNumbersReference.child(slipNumber).removeValue().addOnCompleteListener { removeTask ->
+                    if (removeTask.isSuccessful) {
+                        generateAndSavePdf(slip)
+                        updateSlipCount()
+                        showToast("Data saved successfully")
+                        clearFields()
+                        fetchAndSetCurrentSlipNumber()
+                    } else {
+                        showToast("Failed to remove slip number from available list")
+                    }
+                }
             } else {
                 showToast("Failed to save data")
             }
         }
     }
 
-//    // function to delete slip
-//    private fun deleteSlip(slipNumber: String) {
-//        slipReference.child(slipNumber).removeValue().addOnCompleteListener { task ->
-//            if (task.isSuccessful) {
-//                availableSlipNumbersReference.child(slipNumber).setValue(true)
-//                showToast("Slip deleted successfully")
-//            } else {
-//                showToast("Failed to delete slip")
-//            }
-//        }
-//    }
+    private fun updateSlipCount() {
+        slipCountReference.runTransaction(object : Transaction.Handler {
+            override fun doTransaction(currentData: MutableData): Transaction.Result {
+                var currentCount = currentData.getValue(Int::class.java) ?: 0
+                currentCount += 1
+                currentData.value = currentCount
+                fetchAndSetCurrentSlipNumber()
+                return Transaction.success(currentData)
+            }
 
-    //clear edit texts
+            override fun onComplete(databaseError: DatabaseError?, committed: Boolean, dataSnapshot: DataSnapshot?) {
+                if (databaseError != null) {
+                    showToast("Failed to update slip count: ${databaseError.message}")
+                }
+            }
+        })
+    }
+
+    private fun generateAndSavePdf(slip: Slip) {
+        val pdfDocument = PdfDocument()
+        val pageInfo = PdfDocument.PageInfo.Builder(595, 842, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+        val canvas = page.canvas
+
+        val paint = android.graphics.Paint()
+        paint.textSize = 20f
+        paint.color = Color.BLACK
+
+        canvas.drawText("${binding.companyName.text}", 20f, 25f, paint)
+        canvas.drawText("Slip Number: ${slip.slipNumber}", 80f, 100f, paint)
+        canvas.drawText("Date: ${slip.slipDate}", 80f, 125f, paint)
+        canvas.drawText("Name: ${slip.slipName}", 80f, 150f, paint)
+        canvas.drawText("Vehicle No: ${slip.slipVehicleNo}", 80f, 175f, paint)
+        canvas.drawText("Item: ${slip.slipItem}", 80f, 200f, paint)
+        canvas.drawText("Quantity: ${slip.slipQuantity}", 80f, 225f, paint)
+        canvas.drawText("Amount: ${slip.slipAmount}", 80f, 250f, paint)
+
+        pdfDocument.finishPage(page)
+
+        val filePath = "${getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)}/Slip_${slip.slipNumber}.pdf"
+        val file = File(filePath)
+
+        try {
+            pdfDocument.writeTo(FileOutputStream(file))
+            pdfDocument.close()
+            slip.slipNumber?.let { uploadPdfToFirebase(file, it) }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            showToast("Failed to save PDF")
+        }
+    }
+
+    private fun uploadPdfToFirebase(file: File, slipNumber: String) {
+        val pdfUri = Uri.fromFile(file)
+        val pdfReference = FirebaseStorage.getInstance().reference.child("slips/${file.name}")
+
+        pdfReference.putFile(pdfUri).addOnSuccessListener {
+            pdfReference.downloadUrl.addOnSuccessListener { uri ->
+                val pdfUrl = uri.toString()
+                updateSlipWithPdfUrl(slipNumber, pdfUrl)
+            }
+        }.addOnFailureListener {
+            showToast("Failed to upload PDF")
+        }
+    }
+
+    private fun updateSlipWithPdfUrl(slipNumber: String, pdfUrl: String) {
+        slipReference.child(slipNumber).child("pdfUrl").setValue(pdfUrl).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                showToast("PDF URL saved successfully")
+            } else {
+                showToast("Failed to save PDF URL")
+            }
+        }
+    }
+
+    // Clear edit texts
     private fun clearFields() {
         binding.NameEditText.setText("")
         binding.VehicleEditText.setText("")
         binding.ItemEditText.setText("")
         binding.quantityEditText.setText("")
         binding.amountEditText.setText("")
+        binding.amountEditText.clearFocus()
     }
 
-    //update date
+    // Update date
     private fun updateLabel(myCalendar: Calendar?) {
         val myFormat = "dd/MM/yyyy"
         val sdf = SimpleDateFormat(myFormat, Locale.ROOT)
@@ -158,4 +245,3 @@ class CreateSlip : AppCompatActivity() {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
-
